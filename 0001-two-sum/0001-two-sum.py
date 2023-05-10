505854964b61865875260cff4e2e5b7ba8e1519b
Kevin <64118094+KevinH2003@users.noreply.github.com>
2023-05-10 19:46:02 -0400

    Time: 69 ms (57.59%), Space: 17.9 MB (6.89%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numberDictionary = {}
+        for i in range(len(nums)):
+            numberDictionary[target - nums[i]] = i
+        for j in range(len(nums)):
+            if nums[j] in numberDictionary and j != numberDictionary[nums[j]]:
+                return [numberDictionary[nums[j]], j]
+                
+        
+        
