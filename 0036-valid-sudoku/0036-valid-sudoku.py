9cc242a7a44e58c39faec875e2ba18aaa8fb0db3
Kevin <64118094+KevinH2003@users.noreply.github.com>
2024-05-10 11:59:01 -0400

    Time: 83 ms (93.60%), Space: 16.5 MB (95.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,7 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = [set() for i in range(9)]
         cols = [set() for i in range(9)]
-        boxes = [[set() for i in range(3)] for j in range(3)]
+        boxes = {(i, j): set() for i in range(3) for j in range(3)}
         
         for i in range(len(board)):
             for j in range(len(board[0])):
@@ -15,12 +15,12 @@ class Solution:
                     return False
                 if element in cols[j]:
                     return False
-                if element in boxes[i // 3][j // 3]:
+                if element in boxes[(i // 3, j // 3)]:
                     return False
                     
                 rows[i].add(element)
                 cols[j].add(element)
                 
-                boxes[i // 3][j // 3].add(element)
+                boxes[(i // 3, j // 3)].add(element)
                     
         return True
