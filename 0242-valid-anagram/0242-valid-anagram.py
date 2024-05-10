d3342efadeffc5fa5cb6ba21d0de05414bff85a8
Kevin <64118094+KevinH2003@users.noreply.github.com>
2024-05-10 10:34:51 -0400

    Time: 52 ms (42.54%), Space: 17 MB (64.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,25 +4,16 @@ class Solution:
         tmap = {}
         
         for c in s:
-            if c not in smap:
-                smap[c] = 1
-            else:
-                smap[c] += 1
+            smap[c] = smap.setdefault(c, 0) + 1
         
         for c in t:
-            if c not in tmap:
-                tmap[c] = 1
-            else:
-                tmap[c] += 1
-                
+            tmap[c] = tmap.setdefault(c, 0) + 1
+        
+        if smap.keys() != tmap.keys():
+            return False
+        
         for c in smap.keys():
-            if c not in tmap.keys():
-                return False
             if smap[c] != tmap[c]:
-                return False
-            
-        for c in tmap.keys():
-            if c not in smap.keys():
                 return False
             
         return True
