d82bb2d99d1f4bd3d3d2ee514636bd31674dff15
Kevin <64118094+KevinH2003@users.noreply.github.com>
2024-05-10 11:12:29 -0400

    Time: 81 ms (89.79%), Space: 21.6 MB (25.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        vals = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
-        dic = {}
-        for s in strs:
-            key=1
-            for c in s:
-                key*=vals[ord(c)-ord('a')]
-            if key not in dic:
-                dic[key] = []
-            dic[key].append(s)
-        res=[]
-        for key,val in dic.items():
-            res.append(val)
-        return res
+        anagrams = {}
+        
+        for str in strs:
+            char_appearances = [0] * 26
+            
+            for c in str:
+                char_appearances[ord(c) - ord("a")] += 1
+            
+            anagrams.setdefault(tuple(char_appearances), []).append(str)
+            
+        return list(anagrams.values())
