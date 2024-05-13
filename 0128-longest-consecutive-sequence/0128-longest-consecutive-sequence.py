97e78120735db708422711496d6680215711f423
Kevin <64118094+KevinH2003@users.noreply.github.com>
2024-05-13 11:51:09 -0400

    Time: 2949 ms (35.10%), Space: 31.9 MB (51.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -8,12 +8,12 @@ class Solution:
             if (num - 1) in numset:
                 continue
             
-            sequence_length = 0
             curr = num
             while curr in numset:
-                sequence_length += 1
                 curr += 1
-                
+            
+            sequence_length = curr - num
+            
             if sequence_length > max:
                 max = sequence_length
             
